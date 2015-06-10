f0e6b9ce7a5ef5ddc3a56da2d4eeacde5b1cd2d2
theaisforannie <annharter@gmail.com>
2015-06-10 14:58:01 -0400

    moved everything to index.php, git rm index.html

---
+++ jquerypractice/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Demo</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+		<h1></h1>
+		<a href="http://www.annharter.com">butts</a>
+
+		<?php echo $_GET["name"]; ?>
+
+		<script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+		<script src="js/jquery.js"></script>
+	</body>
+</html>
